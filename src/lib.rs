14f64c40b6cd7c8b8c52b83d727a957728d93d11
EliveltonRepolho <elivelton.repolho@gmail.com>
2022-03-25 11:53:01 -0300

    implementing using libc

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+extern crate libc;
+
+use libc::{c_int, size_t, c_char};
+use std::ffi::CString;
+use std::ptr;
+use std::str;
+use std::slice;
+use std::ffi::CStr;
+
+pub const BUFFER_SIZE: size_t = 255;
+
+#[no_mangle]
+pub extern fn clear_memory(buffer: *mut c_char, size: size_t) {
+    for i in 0..size {
+        unsafe {
+            *buffer.offset(i as isize) = 0;
+        }
+    }
+}
+
+#[no_mangle]
+#[link(name = "acbrposprinter64")]
+extern {
+    fn POS_Inicializar(
+        eArqConfig: *const c_char,
+        eChaveCrypt: *const c_char
+    ) -> c_int;
+    fn POS_Finalizar() -> c_int;
+    fn POS_Versao(
+        sVersao: *mut c_char,
+        esTamanho: *mut size_t
+    ) -> c_int;
+}
+
+pub fn get_version() -> String {
+
+    let ini_file: CString = CString::new( "abcr_lib/ACBrPosPrinter.ini").unwrap();
+    let chave_crypt: CString = CString::new("").unwrap();
+
+    let mut ini_file_pointer: *const c_char = ini_file.as_ptr();
+    let mut chave_crypt_pointer: *const c_char = chave_crypt.as_ptr();
+
+    println!("POS_Inicializar exited with code: {}",
+             unsafe { POS_Inicializar(ini_file_pointer, chave_crypt_pointer) as i32 });
+
+    let mut versao: [c_char; 255] = [0; 255];
+    let mut versao_pointer: *mut c_char = versao.as_mut_ptr();
+    let mut versao_tamanho: size_t = 255;
+
+    println!("POS_Versao exited with code: {}",
+             unsafe { POS_Versao(versao_pointer, &mut versao_tamanho) as i32 });
+
+    let versao_string = unsafe { CStr::from_ptr(versao_pointer) };
+    let versao_string_slice = str::from_utf8(versao_string.to_bytes()).unwrap();
+    let versao_string = versao_string_slice.to_string();
+
+    println!("POS_Finalizar exited with code: {}",
+             unsafe { POS_Finalizar() as i32 });
+
+    // clear memory
+    clear_memory(versao_pointer, versao_tamanho);
+    ini_file_pointer = ptr::null();
+    chave_crypt_pointer = ptr::null();
+
+    return versao_string;
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,11 @@
 use libc::{c_int, size_t, c_char};
-use std::ffi::CString;
-use std::ptr;
-use std::str;
-use std::slice;
-use std::ffi::CStr;
+use std::ffi::{CString,CStr};
+use std::{ptr, str};
+use acbr_rust::{get_version};
 
-#[no_mangle]
-#[link(name = "acbrposprinter64")]
-extern {
-    fn POS_Inicializar(
-        eArqConfig: *const c_char,
-        eChaveCrypt: *const c_char
-    ) -> c_int;
-    fn POS_Finalizar() -> c_int;
-    fn POS_Versao(
-        sVersao: *mut c_char,
-        esTamanho: *mut size_t
-    ) -> c_int;
-}
 
 fn main() {
+    let version = get_version();
+    println!("{}", version);
 
-    let ini_file: CString = CString::new( "abcr_lib/ACBrPosPrinter.ini").unwrap();
-    let chave_crypt: CString = CString::new("").unwrap();
-
-    unsafe {
-        let mut ini_file_pointer: *const c_char = ini_file.as_ptr();
-        let mut chave_crypt_pointer: *const c_char = chave_crypt.as_ptr();
-
-        println!("POS_Inicializar exited with code: {}",
-                 unsafe { POS_Inicializar(ini_file_pointer, chave_crypt_pointer) as i32 });
-
-        let mut versao: [c_char; 255] = [0; 255];
-        let mut versao_pointer: *mut c_char = versao.as_mut_ptr();
-        let mut versao_tamanho: size_t = 255;
-
-        println!("POS_Versao exited with code: {}",
-                 unsafe { POS_Versao(versao_pointer, &mut versao_tamanho) as i32 });
-
-        let versao_string = unsafe { CStr::from_ptr(versao_pointer) };
-        let versao_string_slice = str::from_utf8(versao_string.to_bytes()).unwrap();
-        println!("Versao: {}", versao_string_slice);
-
-        println!("POS_Finalizar exited with code: {}",
-         unsafe { POS_Finalizar() as i32 });
-
-        // clear memory
-        for i in 0..255 {
-            versao[i] = 0;
-        }
-
-
-
-    }
 }
